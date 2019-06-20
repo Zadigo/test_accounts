6f2b0b5bd28f85c9caf26ff154fec66e2b587dbb
John <pendenquejohn@gmail.com>
2019-06-20 15:28:15 +0200

    Improvements

--- kendall/kendall/urls.py
+++ kendall/kendall/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
@@ -7,3 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='hero/hero.html'), name='home'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
