90a5a51f841ed9a82d051ffe10a66fdcb9681cca
John <pendenquejohn@gmail.com>
2019-09-22 10:40:36 +0200

    Code refactoring and separated profile views from registration views

--- kendall/accounts/urls.py
+++ kendall/accounts/urls.py
@@ -2,20 +2,24 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
 
-from accounts import views
+from accounts import views as registration_views
+import views_profile
 
 urlpatterns = [
     url(r'^oauth/', include('social_django.urls', namespace='social')),
-    url(r'^profile/payment-methods/$', views.PaymentMethodsView.as_view(), name='payment_methods'),
+
+    url(r'^profile/payment-methods/$', views_profile.PaymentMethodsView.as_view(), name='payment_methods'),
     # url(r'^profile/personalisation/$', views.PersonalisationView.as_view(), name='personalisation'),
-    url(r'^profile/delete/$', views.ProfileDeleteView.as_view(), name='delete_account'),
-    url(r'^profile/data/$', views.ProfileDataView.as_view(), name='profile_data'),
-    url(r'^profile/change-password/$', views.ChangePasswordView.as_view(), name='change_password'),
+    url(r'^profile/delete/$', views_profile.ProfileDeleteView.as_view(), name='delete_account'),
+    url(r'^profile/data/$', views_profile.ProfileDataView.as_view(), name='profile_data'),
+    url(r'^profile/change-password/$', views_profile.ChangePasswordView.as_view(), name='change_password'),
+    
     url(r'^forgot-password/confirm/(?P<uidb64>[A-Z]+)/(?P<token>\d+\w?\-[a-z0-9]+)', 
-            views.UnauthenticatedChangePasswordView.as_view(), name='password_reset_confirm'),
-    url(r'^forgot-password/$', views.ForgotPasswordView.as_view(), name='forgot_password'),
-    url(r'^login/$', views.LoginView.as_view(), name='login'),
-    url(r'^logout/$', views.LogoutView.as_view(), name='logout'),
-    url(r'^signup/$', views.SignupView.as_view(), name='signup'),
-    url(r'^profile/$', views.ProfileView.as_view(), name='profile'),
+            registration_views.UnauthenticatedChangePasswordView.as_view(), name='password_reset_confirm'),
+    url(r'^forgot-password/$', registration_views.ForgotPasswordView.as_view(), name='forgot_password'),
+    url(r'^login/$', registration_views.LoginView.as_view(), name='login'),
+    url(r'^logout/$', registration_views.LogoutView.as_view(), name='logout'),
+    url(r'^signup/$', registration_views.SignupView.as_view(), name='signup'),
+
+    url(r'^profile/$', views_profile.ProfileView.as_view(), name='profile'),
 ]
--- kendall/accounts/views.py
+++ kendall/accounts/views.py
@@ -1,34 +1,19 @@
 import datetime
 import re
 
-from django.contrib import messages
-from django.contrib.auth import (authenticate, login, logout,
-                                 update_session_auth_hash)
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth.forms import (PasswordChangeForm, PasswordResetForm,
                                        SetPasswordForm)
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.messages import add_message
 from django.core.mail import BadHeaderError, send_mail
-from django.http.response import HttpResponseForbidden
-from django.shortcuts import (Http404, HttpResponse, get_object_or_404,
-                              redirect, render, reverse)
+from django.http.response import Http404, HttpResponse, HttpResponseForbidden
+from django.shortcuts import get_object_or_404, redirect, render, reverse
 from django.utils.translation import gettext
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import View
 
-from accounts.forms import (AddressProfileForm, BaseProfileForm, UserLoginForm,
-                            UserSignupForm)
 from accounts.models import AccountsToken, MyUser, MyUserProfile
-
-
-
-
-
-
-# #####################
-#  REGISTRATION VIEWS
-# #####################
 
 
 class SignupView(View):
@@ -87,23 +72,6 @@ class LogoutView(View):
         return redirect('home')
 
 
-class ChangePasswordView(LoginRequiredMixin, View):
-    def get(self, request, *args, **kwargs):
-        context = {
-            'form': PasswordChangeForm(request.user),
-            'form_button_registration': _('Modifier')
-        }
-        return render(request, 'accounts/profile_password_reset.html', context)
-
-    def post(self, request, **kwargs):
-        form = PasswordChangeForm(request.user, request.POST)
-        if form.is_valid():
-            user = form.save()
-            # IMPORTANT to allow the user to stay logged
-            update_session_auth_hash(request, user)
-        return redirect('/profile/')
-
-
 class ForgotPasswordView(View):
     """This class helps the user reset his password 
     when he has difficulties authenticating
@@ -159,116 +127,3 @@ class UnauthenticatedChangePasswordView(View):
             form.save()
         login(request, user)
         return redirect('/profile/')
-
-
-# #####################
-#   PROFILE VIEWS
-# #####################
-
-class ProfileView(LoginRequiredMixin, View):
-    def get(self, request, *args, **kwargs):
-        user = get_object_or_404(MyUser, id=request.user.id)
-        profile = user.myuserprofile_set.get(myuser=request.user.id)
-
-        context = {
-            'base_profile_form': BaseProfileForm(
-                initial = {
-                    'nom': user.nom,
-                    'prenom': user.prenom
-                }
-            ),
-            'address_profile_form': AddressProfileForm(
-                initial = {
-                    'adresse': profile.adresse,
-                    'ville': profile.ville,
-                    'code_postal': profile.code_postal
-                }
-            )
-        }
-
-        return render(request, 'accounts/profile_profile.html', context)
-
-    def post(self, request, **kwargs):
-        # USING AJAX
-
-        user_id = request.user
-        user = MyUser.objects.get(id=user_id.id)
-        user_profile = user.myuserprofile_set.get(myuser_id_id=user_id.id)
-
-        form_id = request.POST.get('form_id')
-        if form_id == 'base-profile-form':
-            form = BaseProfileForm(request.POST, instance=user)
-
-        if form_id == 'address-profile-form':
-            form = AddressProfileForm(request.POST, instance=user_profile)
-
-        if form.is_valid():
-            form.save()
-
-        return redirect('/profile/')
-
-
-class ProfileDataView(LoginRequiredMixin, View):
-    """Help the user manage his data
-    """
-    def get(self, request, *args, **kwargs):
-        current_user = self.request.user
-
-        # Query the social_auth database and
-        # get a set of connected accounts
-        user = self.request.user.social_auth.get(uid=current_user.email)
-        if user:
-            context = {
-                'provider': user.provider
-            }
-
-        return render(request, 'accounts/profile_data.html', context)
-
-
-class ProfileDeleteView(LoginRequiredMixin, View):
-    """Help the user delete his account
-    """
-    def get(self, request, *args, **kwargs):
-        user = get_object_or_404(MyUser, id=request.user.id)
-        user.delete()
-        return redirect('/')
-
-class PaymentMethodsView(LoginRequiredMixin, View):
-    """Allows the customer to update his/her payment method"""
-    
-    def get(self, request, *args, **kwargs):
-        return render(request, 'accounts/payment_methods.html', {})
-
-    def post(self, request, *kwargs):
-        try:
-            import stripe
-        except ImportError:
-            raise Http404('An error has occured')
-        else:
-            # We can update the customer's cards here
-            # using the stripe api
-            params = {
-                'source': 'source'
-            }
-            stripe.Customer.update(**params)
-        finally:
-            response = {
-                'status': ''
-            }
-        return JsonResponse(response)
-
-# class PersonalisationView(LoginRequiredMixin, View):
-#     def get(self, request, *args, **kwargs):
-#         context = {
-#             'user_profile':self.get_user_profile,
-#             'form': PersonalizationProfileForm
-#         }
-#         return render(request, 'accounts/profile_personalisation.html', context)
-
-#     def post(self, request, **kwargs):
-#         user_profile = self.get_user_profile
-#         redirect('personalisation')
-
-#     @property
-#     def get_user_profile(self):
-#         return MyUserProfile.objects.get(myuser_id_id=self.request.user.id)
